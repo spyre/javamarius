2b59c829432b77dac452834398a7cc5a4db25570
spyre <spirescualex@gmail.com>
2015-12-03 13:03:16 +0200

    TEST FB POST

--- FBTest/src/main/java/app/Config.java
+++ FBTest/src/main/java/app/Config.java
@@ -7,6 +7,10 @@ import facebook4j.Facebook;
 import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
 import facebook4j.Group;
+import facebook4j.PostUpdate;
+import facebook4j.PrivacyBuilder;
+import facebook4j.PrivacyParameter;
+import facebook4j.PrivacyType;
 import facebook4j.conf.ConfigurationBuilder;
 
 public class Config {
@@ -29,12 +33,26 @@ public class Config {
 	public static void main(String[] args) throws FacebookException, MalformedURLException {
 		System.out.println("START");
 		Facebook fb = configurateFB();
-		String groupID = "600339749991037";
-		Group grup = fb.getGroup(groupID); // 423235984489653 // 198200520224963  // machete 600339749991037
-		System.out.println(grup.getDescription());
-		System.out.println(fb.getGroups());
-		URL url = new URL("http://www.emag.ro/welly-lamborghini-gallardo-lp560-4-1-24-wel-2446-lamborghinigallardo/pd/D3S9QBBBM/");
-		fb.postGroupLink(groupID, url);
+//		String groupID = "600339749991037";
+//		Group grup = fb.getGroup(groupID); // 423235984489653 // 198200520224963  // machete 600339749991037
+//		System.out.println(grup.getDescription());
+//		System.out.println(fb.getGroups());
+//		URL url = new URL("http://www.emag.ro/welly-lamborghini-gallardo-lp560-4-1-24-wel-2446-lamborghinigallardo/pd/D3S9QBBBM/");
+
+
+		
+		PrivacyParameter privacy = new PrivacyBuilder().setValue(PrivacyType.ALL_FRIENDS).build();
+		PostUpdate postUpdate = new PostUpdate(new URL("http://facebook4j.org"))
+		    .picture(new URL("http://facebook4j.org/images/hero.png"))
+		    .name("Facebook4J - A Java library for the Facebook Graph API")
+		    .caption("facebook4j.org")
+		    .description("Facebook4J is an unofficial library.")
+		    .privacy(privacy);
+		String postId = fb.postFeed(postUpdate);
+		System.out.println("POST ID: " + postId);
+		System.out.println(postUpdate);
+		
+		
 		
 		System.out.println("END");
 		
